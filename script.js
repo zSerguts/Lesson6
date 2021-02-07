5c59efc816e881c32b421e4e0bbab9a80c6d0049
Zhenya Serguts <z.serguts@gmail.com>
2021-02-07 03:21:55 +0300

    Lesson 6 done.

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+const isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+const number = getRandomInt(100);
+console.log (number);
+function Guess(){
+    let temp = prompt('Угадай число от 1 до 100:');
+    if (typeof(temp) === 'object'){
+        alert('Игра окончена.');
+        return 0;
+    }
+    else if (isNumber(temp) === true){
+        temp = parseInt(temp);
+        if (temp > number){
+            alert('Загаданное число меньше.');
+            return Guess();
+        } else if (temp < number){
+            alert('Загаданное число больше.');
+            return Guess();
+        } else if (temp === number) {
+            alert('Поздравляю, Вы угадали!!!');
+            return 0;
+        }
+    } else {
+        alert('Введи число.');
+        return Guess();
+    }
+}
+
+Guess();
