0d669f70a3b65ba34ae4e090684ba5330afab56d
Zhenya Serguts <z.serguts@gmail.com>
2021-02-08 17:59:33 +0300

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 'use strict';
 
-
-const isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-const number = getRandomInt(100);
-function Guess(){
-    let temp = prompt('Угадай число от 1 до 100:');
-    if (typeof(temp) === 'object'){
-        alert('Игра окончена.');
-        return 0;
-    }
-    else if (isNumber(temp) === true){
-        temp = parseInt(temp);
-        if (temp > number){
-            alert('Загаданное число меньше.');
-            return Guess();
-        } else if (temp < number){
-            alert('Загаданное число больше.');
-            return Guess();
-        } else if (temp === number) {
-            alert('Поздравляю, Вы угадали!!!');
-            return 0;
-        }
-    } else {
-        alert('Введи число.');
-        return Guess();
-    }
-}
-
-Guess();
+function random(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+  
+  let x = +prompt('Введите x:');
+  let y = +prompt('Введите y');
+  
+  for( let i = 0; i < 50; i++){
+      console.log(random(Math.min(x,y), Math.max(x,y)));
+  }
